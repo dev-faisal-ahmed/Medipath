3529bab1e7106a8620404ea29705028fbd3d890c
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-01-06 22:21:58 +0600

    page number added to the table pagination

--- client/src/components/ui/data-table/data-table.tsx
+++ client/src/components/ui/data-table/data-table.tsx
@@ -21,7 +21,7 @@ export function DataTable<TData, TValue>({ columns, data, pagination }: IDataTab
   return (
     <div className="border">
       <Table>
-        <TableHeader className="sticky top-0 bg-neutral-100 shadow">
+        <TableHeader className="sticky top-0 bg-neutral-100">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
@@ -71,23 +71,44 @@ interface ITabelPaginationProps {
 
 function TablePagination({ page, totalPages }: ITabelPaginationProps) {
   const navigate = useNavigate();
+
+  function goNext() {
+    navigate({ search: { page: String(page + 1) } as any });
+  }
+
+  function goPrevious() {
+    navigate({ search: { page: String(page - 1) } as any });
+  }
+
+  function goTo(pageNumber: number) {
+    navigate({ search: { page: String(pageNumber) } as any });
+  }
+
   return (
     <div className="sticky bottom-0 flex h-16 items-center justify-between gap-4 border-t bg-neutral-100 px-4">
-      <Button
-        onClick={() => navigate({ search: { page: String(page - 1) } as any })}
-        disabled={page === 1}
-        variant="outline"
-      >
+      <Button onClick={goPrevious} disabled={page === 1} variant="outline">
         <ArrowLeftIcon /> Previous
       </Button>
-      <div className="font-semibold">
-        {page} / {totalPages}
+      <div className="flex items-center gap-2 font-semibold">
+        {[...Array(totalPages)].map((_, index) => {
+          const pageNumber = index + 1;
+
+          if (pageNumber === page)
+            return (
+              <Button onCanPlay={() => goTo(pageNumber)} variant="outline" size="icon">
+                {pageNumber}
+              </Button>
+            );
+          else if (pageNumber === 1 || pageNumber === totalPages || page + 1 === pageNumber || page - 1 === pageNumber)
+            return (
+              <Button onClick={() => goTo(pageNumber)} variant="ghost" size="icon" key={index}>
+                {pageNumber}
+              </Button>
+            );
+          else return <span> ... </span>;
+        })}
       </div>
-      <Button
-        onClick={() => navigate({ search: { page: String(page + 1) } as any })}
-        disabled={page === totalPages}
-        variant="outline"
-      >
+      <Button onClick={goNext} disabled={page === totalPages} variant="outline">
         Next <ArrowRightIcon />
       </Button>
     </div>
