54c70dcea349f3a232368aa3702b591baef349bd
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-03-01 18:11:13 +0600

    added referrer info into  recent bills

--- client/src/app/(main)/(dashboard)/_ui/recent-bills.tsx
+++ client/src/app/(main)/(dashboard)/_ui/recent-bills.tsx
@@ -2,19 +2,16 @@
 
 import { TOverview } from '@/api-lib/query';
 import { Badge } from '@/components/ui/badge';
+import { ColumnDef } from '@tanstack/react-table';
 import { DataTable } from '@/components/ui/data-table';
 import { formatDate } from '@/helper';
 import { CONST } from '@/lib/const';
 import { TBill } from '@/types';
-import { ColumnDef } from '@tanstack/react-table';
 
 export const RecentBills = ({ bills }: TRecentBillsProps) => {
   const column: ColumnDef<TOverview['bills'][number]>[] = [
     { id: 'serial', header: 'SL.', cell: ({ row }) => <span>{row.index + 1}</span> },
-    {
-      accessorKey: 'billId',
-      header: 'Bill Id',
-    },
+    { accessorKey: 'billId', header: 'Bill Id' },
     {
       id: 'patientInfo',
       header: 'Patient Name',
@@ -24,6 +21,38 @@ export const RecentBills = ({ bills }: TRecentBillsProps) => {
           <p className="mt-1 text-xs font-semibold text-muted-foreground">Age : {row.original.patientInfo.age}</p>
         </div>
       ),
+    },
+    {
+      id: 'Ref',
+      header: 'Ref',
+      cell: ({ row }) => {
+        const agent = row.original.agent;
+        const doctor = row.original.doctor;
+
+        if (!agent && !doctor) return <p className="font-semibold">N/A</p>;
+
+        return (
+          <div>
+            {doctor && (
+              <div className="flex flex-col gap-1">
+                <p>
+                  Visited By : <span className="font-bold">{doctor.name}</span>
+                </p>
+                {doctor.designation && <p className="text-sm text-muted-foreground">{doctor.name}</p>}
+              </div>
+            )}
+
+            {agent && (
+              <div className="flex flex-col gap-1">
+                <p>
+                  Referred By : <span className="font-bold">{agent.name}</span>
+                </p>
+                {agent.designation && <p className="text-sm text-muted-foreground">{agent.designation}</p>}
+              </div>
+            )}
+          </div>
+        );
+      },
     },
     {
       accessorKey: 'services',
